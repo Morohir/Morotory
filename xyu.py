7f8ee23f58c15f1f61e5d225dea3a3f1319eb838
Max <bezbokovboss@mail.ru>
2019-10-25 22:23:01 +0600

    lol

---
+++ xyu.py
@@ -0,0 +1,4 @@
+VladimirVladimirovich = "HUILO"
+print( "PUTIN " + VladimirVladimirovich + "COMPANY" + "!!!")
+otvet = input ( "Вы точно уверены что Путин хуйло? \nОтвет: " )
+print( "Путин хуйло внезависимости от ответа, даже не сомневайтесь!" )
